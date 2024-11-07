c16811df5a2e73dfd3664bda5c9ae0716a25f41e
NikitaM <21960067+Keshoid@users.noreply.github.com>
2024-11-08 01:01:35 +0300

    Revert "Temporary disable signature check"
    This reverts commit 2c6a47c746a28a9db92f208579c6548493878853.

--- src/sign/sign.service.ts
+++ src/sign/sign.service.ts
@@ -269,7 +269,7 @@ export class SignService {
       `Aggregate sign shares for pegout ${pegoutId.toString(16)}`,
     );
     const identifier = validatorKey.validatorKey;
-    /*
+
     const pegoutTxContract = this.tonService.tonClient.open(
       PegoutTxContract.createFromAddress(pegoutRecord.pegoutAddress),
     );
@@ -279,7 +279,6 @@ export class SignService {
       this.logger.log("Completed. Signature already exists.");
       return true;
     }
-    */
     const prevDkg = await this.tcCoordinator.getPrevDKG();
     const pubkeyPackage = prevDkg?.r3Package.pubkeyData?.pubkeyPackage;
 
